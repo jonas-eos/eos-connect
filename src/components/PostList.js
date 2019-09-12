7c0133f7803e9943dc64b3ef5cbdc73b1bfdab97
Jonas Evangelista <jonas.eos@outlook.com>
2019-09-11 23:50:56 -0300

    chore: state basic structure definition

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
 
 class PostList extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        author: {
+          name: "Jonas",
+          avatar: "http://shorturl.at/fyM58"
+        },
+        date: "11 Sep 2019",
+        content:
+          "Hello guys! Get my first job as a nodeJS dev, I'm so happy *.*",
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: "Person 1",
+              avatar: "http://shorturl.at/opLX4"
+            },
+            content: "Nice bro! let's go on :p"
+          }
+        ]
+      }
+    ]
+  };
+
   render() {
-    return;
+    const { posts } = this.state;
+    console.log(posts);
+    return <h1></h1>;
   }
 }
 
