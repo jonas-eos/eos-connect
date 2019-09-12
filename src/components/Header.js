7fc94a4b359ec4570d60c654a7c9a0788b8d9f6c
Jonas Evangelista <jonas.eos@outlook.com>
2019-09-11 23:16:20 -0300

    feature: header

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faIdCard } from "@fortawesome/free-solid-svg-icons";
+
+function Header() {
+  return (
+    <header>
+      <nav className="header-menu">
+        <p className="logo">EOS Connect</p>
+        <div className="profile-div">
+          <span className="profile-label">My Profile</span>
+          <FontAwesomeIcon icon={faIdCard} className="faIdCard" />
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
